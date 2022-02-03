e577b1ef3603080d6d44fe1f2e1dc3660539368e
Carlos Ortiz <carlosor@gmail.com>
2022-02-03 18:03:32 +0100

    add constants from magneto

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,19 +1,19 @@
-export const GAME_TITLE = 'WORDLE CULEGUIL'
+export const WORDLE_TITLE = 'Culegas Wordle'
 
-export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
-export const ABOUT_GAME_MESSAGE = 'About this game'
-export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WIN_MESSAGES = ['Todo ok Jose Luis!', 'Cómo lo quemas!', 'Te has cagado!', 'Has hecho trampas, a Porras vas', 'Un eals para ti!', 'Atesora la victoria!']
+export const GAME_COPIED_MESSAGE = 'Copiado al portapapeles'
+export const ABOUT_GAME_MESSAGE = 'Sobre esta mierda'
+export const NOT_ENOUGH_LETTERS_MESSAGE = 'Pon todas las letras, lerdo'
+export const WORD_NOT_FOUND_MESSAGE = 'Qué dices tontolaba? No existe'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `The word was ${solution}`
-export const ENTER_TEXT = 'Enter'
-export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
-export const NEW_WORD_TEXT = 'New word in'
-export const SHARE_TEXT = 'Share'
-export const TOTAL_TRIES_TEXT = 'Total tries'
-export const SUCCESS_RATE_TEXT = 'Success rate'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+  `Eres muy malo. Sigue jugando en incognito`
+export const ENTER_TEXT = 'Dale'
+export const DELETE_TEXT = 'Borra'
+export const STATISTICS_TITLE = 'Estadísticas'
+export const GUESS_DISTRIBUTION_TEXT = 'Tú qué crees?'
+export const NEW_WORD_TEXT = 'Nueva palabra'
+export const SHARE_TEXT = 'Comparte'
+export const TOTAL_TRIES_TEXT = 'Intentos totales'
+export const SUCCESS_RATE_TEXT = 'Est. de aciertos'
+export const CURRENT_STREAK_TEXT = 'Tu racha'
+export const BEST_STREAK_TEXT = 'Tu mejor intento'
