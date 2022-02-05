735c457db79d2c8c682059e79850442bf50ac9a7
Carlos Ortiz <carlosor@gmail.com>
2022-02-05 12:29:22 +0100

    add 'tequi' and sord wordlist

---
+++ src/constants/wordlist.php
@@ -0,0 +1,42 @@
+<?php
+
+$list = [
+  'ealss',
+  'guaaa',
+  'buuuu',
+  'asgua',
+  'puton',
+  'orras',
+  'chino',
+  'ooort',
+  'grimo',
+  'friti',
+  'chemi',
+  'saooo',
+  'pacop',
+  'kenia',
+  'kiick',
+  'carvo',
+  'efilo',
+  'tetas',
+  'pisil',
+  'cocun',
+  'oruga',
+  'sitio',
+  'hiede',
+  'enano',
+  'caril',
+  'culil',
+  'lloro',
+  'gordo',
+  'gorda',
+  'pacal',
+  'orden',
+  'bilis',
+  'tetil',
+  'tequi'
+];
+
+sort($list);
+
+print_r($list);
